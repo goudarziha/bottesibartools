dab394a55ea31d2ef8b51a818162c83d6e6b07cd
goudarziha <goudarziha@gmail.com>
2024-03-24 17:36:54 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,30 @@ function App() {
     overflow: "hidden",
     color: "white",
   };
+  const textStyle = {
+    margin: "10px",
+  };
   return (
     <div>
       <div style={centerDivStyle}>
         <img src={Botessi} alt="botes" width="500" />
+        <div>
+          <div style={textStyle}>
+            <span>Coming Soon...</span>
+          </div>
+          <div style={textStyle}>
+            <span>
+              We are tirelessly working to get our operations off the ground
+              here in New Orleans
+            </span>
+          </div>
+          <div style={textStyle}>
+            <span>Depp in our incantations.</span>
+          </div>
+          <div style={textStyle}>
+            <span>Thank you for your patience, love and support</span>
+          </div>
+        </div>
       </div>
     </div>
   );
